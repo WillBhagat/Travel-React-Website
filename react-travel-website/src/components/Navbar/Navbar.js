01c5644cfb9648abc677b63e803ff7a3b220426c
WillBhagat <39274234+WillBhagat@users.noreply.github.com>
2023-03-14 17:32:06 +0000

    Adding hero section

--- react-travel-website/src/components/Navbar/Navbar.js
+++ react-travel-website/src/components/Navbar/Navbar.js
@@ -1,51 +1,66 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import './Navbar.css';
-
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Button } from "../Button/Button";
+import "./Navbar.css";
 
 function Navbar() {
-    const [click, setClick] = useState(false);
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
 
-    const handleLick = () => setClick(!click);
-    const closeMenu = () => setClick(!click);
+  const handleLick = () => setClick(!click);
+  const closeMenu = () => setClick(!click);
 
-    return (
-        <>
-            <nav className='navbar'>
-                <div className='navbar-container'>
-                    <Link to='/' className='navbar-logo'>
-                         TRVL
-                    </Link>
-                    <div className='menu-icon' onClick={handleLick}>
-                        <i className={click ? 'fas fa-times' : 'fas fa-bars'}></i>
-                    </div>
-                    <ul className={click ? 'nav=menu active' : 'nav-menu'}>
-                        <li className='nav-item'>
-                            <Link to = "/" className='nav-links' onClick={closeMenu}>
-                                Home
-                            </Link>
-                        </li>
-                        <li className='nav-item'>
-                            <Link to = "/pictures" className='nav-links' onClick={closeMenu}>
-                                Pictures
-                            </Link>
-                        </li>
-                        <li className='nav-item'>
-                            <Link to = "/route" className='nav-links' onClick={closeMenu}>
-                                Route
-                            </Link>
-                        </li>
-                        <li className='nav-item'>
-                            <Link to = "/" className='nav-links-mobile' onClick={closeMenu}>
-                                Sign up
-                            </Link>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
 
-        </>
-    )
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener("resize", showButton);
+
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo" onClick={closeMenu}>
+            TRVL
+          </Link>
+          <div className="menu-icon" onClick={handleLick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
+          </div>
+          <ul className={click ? "nav=menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMenu}>
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/pictures" className="nav-links" onClick={closeMenu}>
+                Pictures
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/route" className="nav-links" onClick={closeMenu}>
+                Route
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/" className="nav-links-mobile" onClick={closeMenu}>
+                Sign up
+              </Link>
+            </li>
+          </ul>
+          {button && <Button buttonStyle="btn--outline">SIGN UP</Button>}
+        </div>
+      </nav>
+    </>
+  );
 }
 
 export default Navbar;
